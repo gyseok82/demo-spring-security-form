aa6b74bc9199610d0b62f0de0fdc54d2b141c9b3
석귀영 <gyseok@bluedigm.com>
2021-06-24 15:08:32 +0900

    spring security : Authentication과 SecurityContextHolder

--- src/main/java/com/example/demospringsecurityform/form/SampleService.java
+++ src/main/java/com/example/demospringsecurityform/form/SampleService.java
@@ -5,6 +5,7 @@ import com.example.demospringsecurityform.account.AccountContext;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
 import java.util.Collection;
@@ -12,8 +13,10 @@ import java.util.Collection;
 @Service
 public class SampleService {
     public void dashboard() {
-        Account account = AccountContext.getAccount();
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        UserDetails userDetails = (UserDetails)authentication.getPrincipal();
         System.out.println("====================");
-        System.out.println(account.getUsername());
+        System.out.println(authentication);
+        System.out.println(userDetails.getUsername());
     }
 }
