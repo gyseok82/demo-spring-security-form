a482e16a0542c9d012ce13fa3f089b6e2671f87c
석귀영 <gyseok@bluedigm.com>
2021-06-25 14:18:26 +0900

    spring security : 스프링 시큐리티 필터와 FilterChainFroxy

--- src/main/java/com/example/demospringsecurityform/config/AnotherSpringSecurity.java
+++ src/main/java/com/example/demospringsecurityform/config/AnotherSpringSecurity.java
@@ -5,28 +5,23 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
-@Order(Ordered.LOWEST_PRECEDENCE - 100)
-public class SpringSecurity extends WebSecurityConfigurerAdapter {
+@Order(Ordered.LOWEST_PRECEDENCE - 15)
+public class AnotherSpringSecurity extends WebSecurityConfigurerAdapter {
 
     @Autowired
     AccountService accountService;
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests()
-                .mvcMatchers("/", "/info", "/account/**").permitAll()
-                .mvcMatchers("/admin").hasRole("ADMIN")
-                .anyRequest().authenticated()
-                .and()
-            .formLogin()
-                .and()
-            .httpBasic();
+        http
+                .antMatcher("/account/**")
+                .authorizeRequests()
+                .anyRequest().permitAll();
     }
 
 }
