e9daddc7b5dfcdb5d7ff0079263e6ca69b2f0f1c
석귀영 <gyseok@bluedigm.com>
2021-07-06 16:41:13 +0900

    spring security : accessDeniedHandler

--- src/main/java/com/example/demospringsecurityform/config/SpringSecurity.java
+++ src/main/java/com/example/demospringsecurityform/config/SpringSecurity.java
@@ -8,6 +8,7 @@ import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
 import org.springframework.security.access.AccessDecisionManager;
 import org.springframework.security.access.AccessDecisionVoter;
+import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.access.expression.SecurityExpressionHandler;
 import org.springframework.security.access.hierarchicalroles.RoleHierarchyImpl;
 import org.springframework.security.access.vote.AffirmativeBased;
@@ -16,11 +17,17 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.access.AccessDeniedHandler;
 import org.springframework.security.web.access.expression.DefaultWebSecurityExpressionHandler;
 import org.springframework.security.web.access.expression.WebExpressionVoter;
 
-import java.nio.file.AccessDeniedException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 import java.util.Arrays;
 import java.util.List;
 
@@ -64,6 +71,18 @@ public class SpringSecurity extends WebSecurityConfigurerAdapter {
         //  http.csrf().disable();
 
         http.logout().logoutSuccessUrl("/");
+
+        // TODO ExceptionTranslatorFilter -> FilterSecurityInterceptor (AccessDecisionManager, AffirmationBased)
+        // TODO AuthenticationException -> AuthenticationEntryPoint
+        // TODO AccessDeniedException -> AccessDeniedHandler
+
+        http.exceptionHandling()
+                .accessDeniedHandler((HttpServletRequest request, HttpServletResponse response, AccessDeniedException e) -> {
+                    UserDetails principal = (UserDetails) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+                    String username = principal.getUsername();
+                    System.out.println(username + " is denied to access " + request.getRequestURI());
+                    response.sendRedirect("/access-denied");
+                });
 
         SecurityContextHolder.setStrategyName(SecurityContextHolder.MODE_INHERITABLETHREADLOCAL);
     }
